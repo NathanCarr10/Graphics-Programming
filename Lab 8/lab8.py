b4429861ce567e53a2f05385fcb2eec4ebfa60df
NathanCarr10 <nathancarr18@gmail.com>
2024-11-18 16:32:16 +0000

    Added blurred image to subplot.

--- Lab 8/lab8.py
+++ Lab 8/lab8.py
@@ -8,21 +8,42 @@ img = cv2.imread('ATU.jpg',)
 img = cv2.imread('ATU.jpg')
 # cv2.imshow('Original', img)
 
+#Creating Kernal Array
+kernel = np.array([[0, 1, 0], 
+                   [1, 1.5, 1], 
+                   [0, 1, 0]])
+
 cv2.waitKey(0)
+
+# Defining Kernal Sizes
+KernelSizeHeight = 15
+KernelSizeWidth = 15
+
+BlurredImage = cv2.GaussianBlur(img,(KernelSizeWidth, KernelSizeHeight),0)
 
 # Use the cvtColor() function to grayscale the image
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # cv2.imshow('Grayscale', gray_image)
 
+# Defining columns and rows
 ncols = 1
-nrows = 2
+nrows = 3
 
+# Original image
 plt.subplot(nrows, ncols,1),plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 plt.title('Original'), plt.xticks([]), plt.yticks([])
 
+# Greyscale image
 plt.subplot(nrows, ncols,2),plt.imshow(gray_image, cmap = 'gray')
 plt.title('GrayScale'), plt.xticks([]), plt.yticks([])
+
+# Blurred image
+plt.subplot(nrows, ncols,3),plt.imshow(cv2.cvtColor(BlurredImage, cv2.COLOR_BGR2RGB))
+plt.title('Blurred Image'), plt.xticks([]), plt.yticks([])
+
 plt.show()
+
+imgOut = cv2.GaussianBlur(imgIn,(KernelSizeWidth, KernelSizeHeight),0)
 
 
 # Keep image on screen
