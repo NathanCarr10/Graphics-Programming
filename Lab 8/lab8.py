f2fc2692c19120c32ab935f30830bbc9ac41d3fb
NathanCarr10 <nathancarr18@gmail.com>
2024-11-18 16:09:26 +0000

    subplotted both images to show original+greyscale

--- Lab 8/lab8.py
+++ Lab 8/lab8.py
@@ -1,17 +1,32 @@
 import cv2
+import numpy as np
+from matplotlib import pyplot as plt
 
 img = cv2.imread('ATU.jpg',)
 
 # Load the input image
 img = cv2.imread('ATU.jpg')
-cv2.imshow('Original', img)
+# cv2.imshow('Original', img)
+
 cv2.waitKey(0)
 
 # Use the cvtColor() function to grayscale the image
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# cv2.imshow('Grayscale', gray_image)
 
-cv2.imshow('Grayscale', gray_image)
-cv2.waitKey(0)  
+ncols = 1
+nrows = 2
+
+plt.subplot(nrows, ncols,1),plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.title('Original'), plt.xticks([]), plt.yticks([])
+
+plt.subplot(nrows, ncols,2),plt.imshow(gray_image, cmap = 'gray')
+plt.title('GrayScale'), plt.xticks([]), plt.yticks([])
+plt.show()
+
+
+# Keep image on screen
+# cv2.waitKey(0)  
 
 # Window shown waits for any key pressing event
-cv2.destroyAllWindows()
+# cv2.destroyAllWindows()
