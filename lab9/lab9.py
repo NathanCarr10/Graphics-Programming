e9f014f82bcd724fbce76be23445e1a7dbcf4e41
NathanCarr10 <nathancarr18@gmail.com>
2024-11-25 20:30:00 +0000

    Shi-Tomasi Corners plotted

--- lab9/lab9.py
+++ lab9/lab9.py
@@ -8,6 +8,9 @@ img = cv2.imread('ATU1.jpg',)
 # Deep Copy of Image
 imgHarris = img.copy()
 
+# mgShiTomasi image copy
+imgShiTomasi = img.copy()
+
 # Defining columns and rows
 ncols = 2
 nrows = 4
@@ -16,7 +19,14 @@ nrows = 4
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Harris Corner detection funcion
-dst = cv2.cornerHarris(gray_image, 2, 3, 0.04)
+dst = cv2.cornerHarris(gray_image, blockSize=2, aperture_size=3, k=0.04)
+
+# Thresholds
+threshold = 0.5; #number between 0 and 1
+for i in range(len(dst)):
+    for j in range(len(dst[i])):
+        if dst[i][j] > (threshold*dst.max()):
+            cv2.circle(imgHarris,(j,i),3,(0, 255, 0),-1)
 
 # Original image
 plt.subplot(nrows, ncols,1),plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
@@ -26,16 +36,22 @@ plt.title('Original'), plt.xticks([]), plt.yticks([])
 plt.subplot(nrows, ncols,2),plt.imshow(gray_image, cmap = 'gray')
 plt.title('GrayScale'), plt.xticks([]), plt.yticks([])
 
-# Corner Dection image
-plt.subplot(nrows, ncols,3),plt.imshow(gray_image, cmap = 'gray')
-plt.title('Corner Detection'), plt.xticks([]), plt.yticks([])
+# Corner Dection/ Harris image
+plt.subplot(nrows, ncols,3),plt.imshow(cv2.cvtColor(imgHarris, cv2.COLOR_BGR2RGB))
+plt.title('Harris'), plt.xticks([]), plt.yticks([])            
 
-# Thresholds
-threshold = 0.5; #number between 0 and 1
-for i in range(len(dst)):
-    for j in range(len(dst[i])):
-        if dst[i][j] > (threshold*dst.max()):
-            cv2.circle(imgHarris,(j,i),3,(2, 10, 50),-1)
+
+# Good Features To Track
+corners = cv2.goodFeaturesToTrack(gray_image, maxCorners, qualityLevel, minDistance)
+
+# GFTT corners loop through the corners array
+for i in corners:
+    x,y = i.ravel()
+    cv2.circle(imgShiTomasi,(x,y),5,(255, 0, 0),-1)  
+
+#ShiTomasi image plot
+plt.subplot(nrows, ncols, 4)plt.imshow(cv2.cvtColor(imgShiTomasi, cv2.COLOR_BGR2RGB))
+plt.title('Shi-Tomasi Corners')plt.xticks([]), plt.yticks([])
 
 
 plt.show()
